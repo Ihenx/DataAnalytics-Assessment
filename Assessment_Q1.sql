df235abb5fd529abf77ee05868ae0d34c2088e8f
God'spower <66071052+Ihenx@users.noreply.github.com>
2025-06-07 10:16:37 +0100

    Add files via upload
    Modified the code

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,20 +1,26 @@
-SELECT 
+with user_plan_data as (SELECT 
+distinct
     u.id AS owner_id,
+    p.id as plan_id,
     CONCAT_WS(' ', u.first_name, u.last_name) AS full_name,
-    COUNT(CASE
-        WHEN p.is_regular_savings >= 1 THEN p.id
-    END) AS savings_count,
-    COUNT(CASE
-        WHEN p.is_fixed_investment >= 1 THEN p.id
-    END) AS investment_count,
-    ROUND(SUM(s.amount), 0) AS transaction_deposit
+    p.is_regular_savings as savings_account,
+    p.is_fixed_investment as investment_account,
+    s.amount as transaction_amount
 FROM
     users_customuser u
         LEFT JOIN
     plans_plan p ON u.id = p.owner_id
         LEFT JOIN
     savings_savingsaccount s ON p.id = s.plan_id
-GROUP BY u.id , CONCAT_WS(' ', u.first_name, u.last_name)
-HAVING savings_count >= 1
-    AND investment_count >= 1
+    )
+    
+    select owner_id,
+			full_name,
+            count(case when coalesce(savings_account,0) >= 1 then plan_id end ) as saving_count,
+            count(case when coalesce(investment_account,0) >= 1 then plan_id end) as investment_count,
+            round(sum(transaction_amount),0) as transaction_deposit
+	from user_plan_data
+    group by owner_id, full_name
+    having saving_count >= 1 and investment_count >= 1
     order by transaction_deposit desc;
+    
