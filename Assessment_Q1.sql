0aa8ced58b751e9d9008d5f99165da8594a6af99
God'spower <66071052+Ihenx@users.noreply.github.com>
2025-06-07 10:34:10 +0100

    Add files via upload

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,8 +1,9 @@
+-- Define a Common Table Expression (CTE) to show user, plan, and transaction data
 with user_plan_data as (SELECT 
 distinct
     u.id AS owner_id,
     p.id as plan_id,
-    CONCAT_WS(' ', u.first_name, u.last_name) AS full_name,
+    CONCAT_WS(' ', u.first_name, u.last_name) AS full_name, -- Concatenates first and last name
     p.is_regular_savings as savings_account,
     p.is_fixed_investment as investment_account,
     s.amount as transaction_amount
@@ -13,11 +14,11 @@ FROM
         LEFT JOIN
     savings_savingsaccount s ON p.id = s.plan_id
     )
-    
+    -- Main query: Aggregate data per user
     select owner_id,
 			full_name,
-            count(case when coalesce(savings_account,0) >= 1 then plan_id end ) as saving_count,
-            count(case when coalesce(investment_account,0) >= 1 then plan_id end) as investment_count,
+            count(case when savings_account >= 1 then plan_id end ) as saving_count, -- Count of savings plans per user
+            count(case when investment_account >= 1 then plan_id end) as investment_count, --  Count of savings plans per user
             round(sum(transaction_amount),0) as transaction_deposit
 	from user_plan_data
     group by owner_id, full_name
